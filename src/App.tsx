f9563ebc9f3175c765e181d8440b83d6d9c27df6
Gustavo Buziki <gustavo.buziki@paipe.co>
2024-02-07 13:33:23 -0300

    feat: notes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard, NoteCard } from "./components";
 
 export function App() {
   return (
-    <div>
-      <img src={logo}></img>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW Expert"></img>
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard />
+        <NoteCard date={new Date()} content="Hello World" />
+      </div>
     </div>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import { NoteCard } from "./note-card";
+import { NewNoteCard } from "./new-note-card";
+
+export { NoteCard, NewNoteCard };
